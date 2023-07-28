511c6bfc11a65bc0532dde93b3dcd1d890193eb4
poojprab <prabu.p@northeastern.edu>
2023-07-28 17:39:23 -0400

    Implemented and deployed our bot to handle the user's playing wordle via chat. Fixes #2, #3, #4, #5, #6

--- bot.js
+++ bot.js
@@ -3,11 +3,22 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const client = new Client({ intents: [
-    'Guilds', 'GuildMessages', 'MessageContent']})
+const client = new Client({
+    intents: [
+        'Guilds', 'GuildMessages', 'MessageContent']
+})
+
+const channel = client.channels.cache.get('1133762136917676145');
 
 const axios = require('axios');
+const {response} = require("express");
+const fs = require('fs');
+const readline = require('readline');
+const {stringify} = require("nodemon/lib/utils");
 
+let guesses = [];
+let wordleStart = false;
+let correctWord = '';
 
 
 client.once(Events.ClientReady, c => {
@@ -15,20 +26,127 @@ client.once(Events.ClientReady, c => {
 });
 
 
-client.on('messageCreate', msg => {
+client.on('messageCreate', async (msg) => {
+    if (msg.author.id === client.user.id) {
+        return;
+    }
     if (msg.content.includes("hi duck")) {
-        msg.reply("hi zoe and pooja")
-        //Example using Axios to get JSON data from a server
-        axios.get('http://localhost:3000/data.json')
-            .then((response) => {
-                // response.data will contain the parsed JSON data
-                console.log(response.data);
-            })
-            .catch((error) => {
-                console.error('Error fetching JSON:', error);
+        await msg.reply("hi zoe and pooja")
+    }
+    if (msg.content.startsWith("!play")) {
+        await msg.reply({
+            content: 'Select Games',
+            components: [gameMenu]
+        });
+    }
+    if (msg.content.startsWith("!guess") && wordleStart) {
+        const channel = client.channels.cache.get('1133762136917676145');
+        const currentGuess = (msg.content.substring(msg.content.indexOf("!guess") + 6)).trim().toLowerCase();
+        if (currentGuess.length < 5 || currentGuess.length > 5) {
+            await msg.reply("Invalid Guess");
+        } else {
+            guesses.unshift(currentGuess)
+            await msg.reply("Your guesses so far: " + guesses);
+        }
+
+        console.log("LENGTH OF THE ARRAY: " + guesses.length)
+
+
+        for (let i = guesses.length; i > 0; i--) {
+            console.log("HI")
+            channel.send({ content: ' ', components: [generateRow(guesses[i - 1])] });
+        }
+
+        for (let i = 0; i < (5 - guesses.length); i++) {
+            channel.send({ content: ' ', components: [row2] });
+        }
+
+        if (guesses[0] === correctWord) {
+            channel.send('you won !');
+            guesses = []
+            wordleStart = false;
+        } else if (guesses.length === 5 && guesses[0] !== correctWord) {
+            channel.send('you lost :(')
+            guesses = []
+            wordleStart = false;
+        }
+    }
+})
+
+function generateRow(guessedWord) {
+    const row3 = new ActionRowBuilder();
+
+    for (let i = 0; i < 5; i++)  {
+        const cwLetter = correctWord.charAt(i)
+        const gwLetter = guessedWord.charAt(i)
+        console.log(cwLetter + " " + gwLetter + " " + i)
+        if (cwLetter === gwLetter) {
+            row3.addComponents(new ButtonBuilder()
+                .setCustomId('gradedButtonGreen' + i)
+                .setLabel(guessedWord.charAt(i).toUpperCase())
+                .setStyle(3)
+                .setDisabled(true))
+        } else if (correctWord.includes(gwLetter)) {
+            row3.addComponents(new ButtonBuilder()
+                .setCustomId('gradedButtonBlue' + i)
+                .setLabel(guessedWord.charAt(i).toUpperCase())
+                .setStyle(1)
+                .setDisabled(true));
+        } else {
+            row3.addComponents(new ButtonBuilder()
+                .setCustomId('gradedButtonGrey' + i)
+                .setLabel(guessedWord.charAt(i).toUpperCase())
+                .setStyle(2)
+                .setDisabled(true))
+        }
+    }
+    return row3;
+}
+
+client.on('interactionCreate', async (interaction) => {
+    if (interaction.customId === 'playWordleButton') {
+        interaction.reply("Game has started")
+        wordle().then( (randomWord) => {
+            const channel = client.channels.cache.get('1133762136917676145');
+            for (let i = 0; i < 5; i++) {
+                channel.send({ content: ' ', components: [row2] });
+            }
+            wordleStart = true;
+            //playWordleButton.setDisabled(true);
+            correctWord = randomWord;
+        })
+            .catch((err) => {
+                console.error('Error:', err);
             });
     }
 })
+
+function wordle() {
+    return new Promise((resolve, reject) => {
+        read((err, data) => {
+            if (err) {
+                console.error('Error:', err);
+                reject(err);
+            } else {
+                const wordsArray = data.trim().split('\n');
+                const randomNum = Math.floor(Math.random() * wordsArray.length);
+                resolve(wordsArray[randomNum]);
+            }
+        });
+    });
+}
+
+function read(callback) {
+    fs.readFile('nyt.txt', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading the file:', err);
+            callback(err, null);
+        } else {
+            callback(null, data);
+        }
+    });
+}
+
 
 //npm run devStart
 
--- src/main/java/starterCode/Driver.java
+++ src/main/java/starterCode/Driver.java
@@ -10,30 +10,5 @@ public class Driver {
     public static void main(String[] args) {
         System.out.println("help");
         String jsonData = "{\"name\":\"John Doe\", \"age\":30, \"occupation\":\"Developer\"}";
-
-        try {
-            // Replace 'http://localhost:3000/receiveData' with the actual server URL and route
-            URL url = new URL("http://localhost:3000/data.json");
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-            // Set request method to POST
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "application/json");
-            connection.setDoOutput(true);
-
-            // Write the JSON data to the server
-            try (OutputStream outputStream = connection.getOutputStream()) {
-                byte[] input = jsonData.getBytes(StandardCharsets.UTF_8);
-                outputStream.write(input, 0, input.length);
-            }
-
-            // Read the response from the server (optional)
-            int responseCode = connection.getResponseCode();
-            System.out.println("Response Code: " + responseCode);
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
