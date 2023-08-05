b19bbc0827c18d6d2245d869d9c1980e1b5816ed
poojprab <prabu.p@northeastern.edu>
2023-08-05 17:48:55 -0400

    Started on Anagrams implementation

--- bot.js
+++ bot.js
@@ -25,12 +25,13 @@ let wordleStart = false;
 let correctWord = '';
 
 let twentyQsStart = false;
+let anagramStart = false;
 let setupPlayers = false;
 
 let gameChannel = '';
 
-let questioner = ''
-let questionee = ''
+let player1 = ''
+let player2 = ''
 let secretWord = ''
 let currentQNum = 1;
 
@@ -38,6 +39,16 @@ let currentQNum = 1;
 client.once(Events.ClientReady, c => {
     console.log('Bot is ready to speak!!!');
     gameChannel = client.channels.cache.get('1133762136917676145');
+
+    //this code line will use the api to get the definition and if it doens't exist it will say:
+    // {
+    //     title: 'No Definitions Found',
+    //     message: "Sorry pal, we couldn't find definitions for the word you were looking for.",
+    //     resolution: 'You can try the search again at later time or head to the web instead.'
+    // }
+
+
+    //fetch(url).then(res => res.json()).then(result => isInDictionary(result));
 });
 
 const playWordleButton = new ButtonBuilder()
@@ -64,6 +75,43 @@ for (let i = 0; i < 5; i++) {
         .setDisabled(true));
 }
 
+let anagramList = ['', ''];
+
+async function settingUpPlayers(senderID, gameType, msg) {
+    console.log("players are currently being setup")
+    // Respond to the private message
+    if (msg.author.id === client.user.id) {
+        return;
+    }
+
+    const opponent = fetchId(msg.content.trim())
+
+    console.log(opponent);
+    if (gameChannel.guild.members.cache.has(opponent)
+        && opponent !== '1133762136917676145') {
+        player1 = senderID;
+        player2 = opponent;
+        await client.users.send(player1, 'Your opponent is: ' +
+            msg.content + '\n Request Sent! Waiting for opponent response');
+        await client.users.send(opponent,{
+            content: msg.author.username.toString() + ' has challenged you to' + gameType,
+            components: [new ActionRowBuilder()
+                .addComponents(new ButtonBuilder()
+                    .setCustomId('accept20')
+                    .setLabel('Accept the Challenge')
+                    .setStyle(1))
+                .addComponents(new ButtonBuilder()
+                    .setCustomId('reject20')
+                    .setLabel('Reject the Challenge')
+                    .setStyle(2))
+            ]
+        } )
+    } else {
+        await msg.author.send('This user are not available to play :( ')
+    }
+    setupPlayers = false;
+}
+
 client.on('messageCreate', async (msg) => {
 
     if (msg.channel instanceof DMChannel) {
@@ -71,52 +119,35 @@ client.on('messageCreate', async (msg) => {
             return;
         }
         console.log("received dm")
-        if (twentyQsStart === true) {
-            console.log("twenty questions as started (button clicked)")
-            if (setupPlayers === true) {
-                console.log("players are currently being setup")
-                // Respond to the private message
-                if (msg.author.id === client.user.id) {
-                    return;
-                }
-
-                const opponent = fetchId(msg.content.trim())
-
-                console.log(opponent);
-                if (gameChannel.guild.members.cache.has(opponent)
-                    && opponent !== '1133762136917676145') {
-                    questioner = msg.author.id;
-                    questionee = opponent;
-                    await msg.author.send('Your opponent is: ' +
-                        msg.content + '\n Request Sent! Waiting for opponent response');
-                    //await client.users.send(opponent, msg.author.username.toString() + ' has challenged you to 20 questions')
-                    await client.users.send(opponent,{
-                        content: msg.author.username.toString() + ' has challenged you to 20 questions',
-                        components: [new ActionRowBuilder()
-                            .addComponents(new ButtonBuilder()
-                                .setCustomId('accept20')
-                                .setLabel('Accept the Challenge')
-                                .setStyle(1))
-                            .addComponents(new ButtonBuilder()
-                                .setCustomId('reject20')
-                                .setLabel('Reject the Challenge')
-                                .setStyle(2))
-                        ]
-                    } )
-                } else {
-                    await msg.author.send('This user are not available to play :( ')
-                }
-                setupPlayers = false;
+        if (setupPlayers === true) {
+            if (twentyQsStart) {
+                await settingUpPlayers(msg.author.id, '20 Question', msg)
             } else {
+                await settingUpPlayers(msg.author.id, 'Anagrams', msg)
+            }
+        } else {
+            if (anagramStart === true) {
+                console.log(anagramList)
+
+                if (anagramList.includes((msg.content.trim().toLowerCase()))) {
+                    console.log("good word")
+                    await msg.react("👍")
+                } else {
+                    console.log("bad word")
+                    await msg.react("👎")
+                }
+            }
+            if (twentyQsStart === true) {
                 if (secretWord === '') {
                     console.log("players have been setup, choosing secret word")
+                    await client.users.send(player1, 'Enter secret word: ')
                     secretWord = msg.content.trim();
                     console.log("Secret Word is: " + secretWord)
-                    await client.users.send(questioner, "Waiting for first question...");
-                    await client.users.send(questionee, "Opponent has chosen a word! Ask question or enter the correct word");
+                    await client.users.send(player1, "Waiting for first question...");
+                    await client.users.send(player2, "Opponent has chosen a word! Ask question or enter the correct word");
                 } else {
-                    if (msg.author.id === questionee && currentQNum < 21) {
-                        await client.users.send(questioner, {
+                    if (msg.author.id === player2 && currentQNum < 21) {
+                        await client.users.send(player1, {
                                 content: "Question " + currentQNum + ": " + msg.content + "?",
                                 components: [new ActionRowBuilder()
                                     .addComponents(new ButtonBuilder()
@@ -137,12 +168,12 @@ client.on('messageCreate', async (msg) => {
                                         .setStyle(3))
                                 ]
                             }
-                            );
+                        );
                     }
                 }
             }
         }
-    }
+        }
 
     if (msg.content.includes("hi duck")) {
         await msg.reply("hi zoe and pooja")
@@ -181,8 +212,63 @@ client.on('messageCreate', async (msg) => {
             wordleStart = false;
         }
     }
+
+    if (msg.content.startsWith("!guess")) {
+        const currentGuess = (msg.content.substring(msg.content.indexOf("!guess") + 6)).trim().toLowerCase();
+        main(currentGuess);
+        //fetch(url+currentGuess).then(res => res.json()).then(result => isInDictionary(result));
+    }
 })
 
+async function checkIfInDictionary(word) {
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.title; // Assuming the API returns an object with a property indicating if the word is in the dictionary
+}
+
+function generateAnagrams(word, length) {
+    if (length === 0) {
+        return [''];
+    }
+
+    const anagrams = [];
+    for (let i = 0; i < word.length; i++) {
+        const char = word[i];
+        const remainingChars = word.slice(0, i) + word.slice(i + 1);
+        const subAnagrams = generateAnagrams(remainingChars, length - 1);
+        for (const subAnagram of subAnagrams) {
+            anagrams.push(char + subAnagram);
+        }
+    }
+    return anagrams;
+}
+
+async function main(input) {
+    const inputLetters = input.toLowerCase().replace(/\s/g, '');
+    console.log(inputLetters)
+
+    const anagrams3 = generateAnagrams(inputLetters, 3);
+    const anagrams4 = generateAnagrams(inputLetters, 4);
+    //const anagrams5 = generateAnagrams(inputLetters, 5);
+    const newArray = anagrams3.concat(anagrams4);
+
+
+    console.log("\n3-Letter Anagrams in Dictionary:");
+    let x = 0;
+
+    let actualAnagram = []
+    for (const anagram of newArray) {
+        const isInDictionary = await checkIfInDictionary(anagram);
+        if (!isInDictionary) {
+            actualAnagram.push(anagram);
+            console.log(x + anagram);
+            x++
+        }
+    }
+    console.log(actualAnagram)
+    return actualAnagram;
+}
 
 function fetchId(desiredUserName) {
 
@@ -261,24 +347,31 @@ client.on('interactionCreate', async (interaction) => {
 
     if (interaction.customId === 'reject20') {
         twentyQsStart = false;
-        await client.users.send(questioner, (await client.users.fetch(questionee)).username + ' has rejected your request :(');
+        await client.users.send(player1, (await client.users.fetch(player2)).username + ' has rejected your request :(');
         interaction.reply('successfully rejected game request')
         setupPlayers = false;
     }
     if (interaction.customId === 'accept20') {
-        await client.users.send(questioner, (await client.users.fetch(questionee)).username + ' has accepted your request\n')
-        await client.users.send(questioner, 'Enter secret word: ')
-        interaction.reply('successfully joined the game, please wait while opponent choose their secret word')
+        await client.users.send(player1, (await client.users.fetch(player2)).username + ' has accepted your request\n')
+        interaction.reply('successfully joined the game! Please wait')
+
+        if (anagramStart === true) {
+            await client.users.send(player1, 'The Letters Are:' + randomLetter.join(' ')
+                + '\n Type Your Anagrams!' );
+            await client.users.send(player2, 'The Letters Are:' + randomLetter.join(' ')
+                + '\n Type Your Anagrams!' );
+        }
+
         setupPlayers = false;
     }
 
     if (interaction.customId === 'Yes20') {
         isTwentyQsOver();
-        await client.users.send(questionee, "Question " + currentQNum + " Answer: Yes")
+        await client.users.send(player2, "Question " + currentQNum + " Answer: Yes")
         currentQNum++
         if (isTwentyQsOver()) {
             interaction.reply("They couldn't guess it! You won!")
-            await client.users.send(questionee, "You ran out of guesses! You Lost !\n"
+            await client.users.send(player2, "You ran out of guesses! You Lost !\n"
                 + "The secret word was: " + secretWord);
         } else {
             interaction.reply("You chose Yes. Waiting for new questions...")
@@ -286,11 +379,11 @@ client.on('interactionCreate', async (interaction) => {
     }
 
     if (interaction.customId === 'No20') {
-        await client.users.send(questionee, "Question " + currentQNum + " Answer: No")
+        await client.users.send(player2, "Question " + currentQNum + " Answer: No")
         currentQNum++
         if (isTwentyQsOver()) {
             interaction.reply("They couldn't guess it! You won!")
-            await client.users.send(questionee, "You ran out of guesses! You Lost !\n"
+            await client.users.send(player2, "You ran out of guesses! You Lost !\n"
                 + "The secret word was: " + secretWord);
         } else {
             interaction.reply("You chose No. Waiting for new questions...")
@@ -299,11 +392,11 @@ client.on('interactionCreate', async (interaction) => {
 
     if (interaction.customId === 'Sometimes20') {
         isTwentyQsOver();
-        await client.users.send(questionee, "Question " + currentQNum + " Answer: Maybe")
+        await client.users.send(player2, "Question " + currentQNum + " Answer: Maybe")
         currentQNum++
         if (isTwentyQsOver()) {
             interaction.reply("They couldn't guess it! You won!")
-            await client.users.send(questionee, "You ran out of guesses! You Lost !\n"
+            await client.users.send(player2, "You ran out of guesses! You Lost !\n"
                 + "The secret word was: " + secretWord);
         } else {
             interaction.reply("You chose Maybe. Waiting for new questions...")
@@ -313,7 +406,7 @@ client.on('interactionCreate', async (interaction) => {
     if (interaction.customId === 'YouGotIt') {
         isTwentyQsOver();
         interaction.reply("They got it! You lost :(")
-        await client.users.send(questionee, "You guessed it right! You Won !")
+        await client.users.send(player2, "You guessed it right! You Won !")
         twentyQsStart = false;
         currentQNum = 1;
     }
@@ -322,8 +415,8 @@ client.on('interactionCreate', async (interaction) => {
 
 function isTwentyQsOver() {
     if (currentQNum === 21) {
-        // client.users.send(questioner, "They couldn't guess! You Won !")
-        // client.users.send(questionee, "You ran out of guesses! You Lost !\n"
+        // client.users.send(player1, "They couldn't guess! You Won !")
+        // client.users.send(player2, "You ran out of guesses! You Lost !\n"
         //     + "The secret word was: " + secretWord);
         twentyQsStart = false;
         currentQNum = 1;
